5689ed855a6db5d4f96eb662feacb2624d440ea1
EnatMathews <enatmathews@gmail.com>
2024-07-29 21:34:32 +0530

    project created

--- app.js
+++ app.js
@@ -11,7 +11,39 @@ app.use(Cors())
 
 Mongoose.connect("mongodb+srv://Enat:EnatVibin@cluster0.ts1wpg0.mongodb.net/blogDb?retryWrites=true&w=majority&appName=Cluster0")
 
+//Sign Up
 
+app.post("/signIn",async(req,res)=>{
+    let input=req.body
+    let result=userModel.find({email:req.body.email}).then((items)=>{
+        if (items.length>0) {
+            const passwordValidatior=Bcrypt.compareSync(req.body.password,items[0].password)
+            if (passwordValidatior) {
+                jwt.sign({email:req.body.email},"blogApp",{expiresIn:"1d"},(error,token)=>{
+                    if (error) {
+                        res.json({"status":"error","error":error})
+                    } else {
+                        res.json({"status":"success","token":token,"userId":items[0]._id})
+                        
+                    }
+                })                
+            } else {
+                
+                res.json({"status":"Invalid password"}
+
+                )}
+            
+        } else {
+            res.json({"status":"Invalid email id"})
+        }
+    }).catch()
+
+})
+
+
+
+
+// Sign Up
 app.post("/signup",async(req,res)=>{
     
     let input = req.body
@@ -37,6 +69,6 @@ app.post("/signup",async(req,res)=>{
  )
    
 })
-app.listen(8181,()=>{
+app.listen(8888,()=>{
     console.log("Server started")
 })
