909b61712a235f4b0c1b2c094f20cdf6645862e4
EnatMathews <enatmathews@gmail.com>
2024-07-29 22:38:01 +0530

    project created

--- app.js
+++ app.js
@@ -4,12 +4,38 @@ const Bcrypt = require("bcrypt")
 const Cors = require("cors")
 const jwt = require("jsonwebtoken")
 const  userModel=require("./models/users")
-
+const postModel = require("./models/posts")
 let app = Express()
 app.use(Express.json())
 app.use(Cors())
 
 Mongoose.connect("mongodb+srv://Enat:EnatVibin@cluster0.ts1wpg0.mongodb.net/blogDb?retryWrites=true&w=majority&appName=Cluster0")
+// create a post
+
+app.post("/create", async (req, res) => {
+    let input = req.body;
+    let token = req.headers.token;
+
+    jwt.verify(token, "blogApp", async (error, decoded) => {
+        if (error) {
+            return res.json({ "status": "Invalid Authentication" });
+        }
+
+        if (decoded && decoded.email) {
+            try {
+                let result = new postModel(input);
+                await result.save();
+                res.json({ "status": "Success" });
+            } catch (err) {
+                res.json({ "status": "Error", "message": err.message });
+            }
+        } else {
+            res.json({ "status": "Invalid Authentication" });
+        }
+    });
+});
+
+
 
 //Sign Up
 
@@ -69,6 +95,6 @@ app.post("/signup",async(req,res)=>{
  )
    
 })
-app.listen(8888,()=>{
+app.listen(8999,()=>{
     console.log("Server started")
 })
